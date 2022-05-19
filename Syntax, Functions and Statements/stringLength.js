e71496c039276cd58aeabf268a5dc5ba3a6ba444
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-05-19 15:36:56 +0100

    Create stringLength.js

---
+++ Syntax, Functions and Statements/stringLength.js
@@ -0,0 +1,6 @@
+function stringLength(str1, str2, str3){
+    let sum = str1.length + str2.length + str3.length;
+    console.log(sum);
+    let avgLength = Math.floor(sum / 3);
+    console.log(avgLength);
+}
