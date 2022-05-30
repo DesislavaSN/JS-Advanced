e7bc15ee2db66dad7bc64f78cde44598cc181513
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-05-30 17:41:36 +0100

    Update solution.js

--- DOM Introduction/Exercises/5.Table/solution.js
+++ DOM Introduction/Exercises/5.Table/solution.js
@@ -1,16 +1,15 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
+   const tableRows = Array.from(document.getElementsByTagName('tr'));
+   const search = document.getElementById('searchField');
+   
    function onClick() {
-      const tableRows = Array.from(document.getElementsByTagName('tr'));
-      const search = document.getElementById('searchField');
       for(let i = 2; i < tableRows.length; i++){
          if (tableRows[i].textContent.includes(search.value)) {
-            // console.log('true');
             tableRows[i].classList.add("select");
-            // tableRows[i].classList.remove("select"); // ?????
-
-         }
+         } else {
+            tableRows[i].classList.remove("select");
       }
       search.value = '';
    }
