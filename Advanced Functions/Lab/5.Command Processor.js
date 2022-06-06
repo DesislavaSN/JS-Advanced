6e19c399dfebc9918f302afb24a2b1a123e05f3f
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-06-06 12:09:10 +0100

    Update 5.Command Processor.js

--- Advanced Functions/Lab/5.Command Processor.js
+++ Advanced Functions/Lab/5.Command Processor.js
@@ -1,8 +1,6 @@
 // 5. Command Processor
 
 function commandProcessor() {
-  // CLOSURE e promenlivata 'state' za tazi f.
-  // 'state' promenlivata e dostupna za vsichki functions nadoly v golemia scope na commandProcessor()
   let str = "";
 
   function append(text) {
@@ -21,25 +19,16 @@ function commandProcessor() {
     console.log(str);
   }
 
-  // vrushtame object koito ima refernecii kum vsichkite functions koito sa v glavnata f. commandProcessor(),
-  // tova moje da go narichame methods poneje te manipolirat nqkakvi danni koito sa vurzani kum functions
   const result = {
     append,
     removeStart,
     removeEnd,
     print,
   };
+  
   return result;
-  // tova e suhtoto kato gornite redove - vmesto da suzdavame object i da go vrushtame, to go vrushtame directno!
-  //   return {
-  //     append,
-  //     removeStart,
-  //     removeEnd,
-  //     print,
-  //   };
 }
 
-// s const test = commandProcessor(); - > suzdavame procesora i go testvame
 const test = commandProcessor();
 test.append("hello");
 test.append("again");
