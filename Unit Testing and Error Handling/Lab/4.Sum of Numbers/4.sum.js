c69d05e2b9b03a5b9fae7013f01e59acceda5a22
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-06-12 17:33:24 +0300

    Add files via upload

---
+++ Unit Testing and Error Handling/Lab/4.Sum of Numbers/4.sum.js
@@ -0,0 +1,14 @@
+// 4.	Sum of Numbers
+
+function sum(arr) {
+    let sum = 0;
+    for (let num of arr){
+        sum += Number(num);
+    }
+    return sum;
+}
+
+
+module.exports = {
+    sum,
+}
