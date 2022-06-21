4296bf122e009325f4bad6a500a0d35db58e7d25
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-06-21 10:33:41 +0300

    Add files via upload

---
+++ Exams/Exam 6 Apr 2022/02.garden.js
@@ -0,0 +1,142 @@
+// 2.  Garden
+
+class Garden {
+  constructor(spaceAvailable) {
+    this.spaceAvailable = spaceAvailable;
+    this.plants = [];
+    this.storage = [];
+  }
+
+  addPlant(plantName, spaceRequired) {
+    if (this.spaceAvailable < spaceRequired) {
+      throw new Error("Not enough space in the garden.");
+    }
+    this.spaceAvailable -= spaceRequired;
+    const plant = {
+      plantName,
+      spaceRequired,
+      ripe: false,
+      quantity: 0,
+    };
+
+    this.plants.push(plant);
+    return `The ${plantName} has been successfully planted in the garden.`;
+  }
+
+  ripenPlant(plantName, quantity) {
+    if (quantity <= 0) {
+      throw new Error(`The quantity cannot be zero or negative.`);
+    }
+
+    const plantFound = this.plants.find((p) => p.plantName == plantName);
+    if (plantFound == undefined) {
+      throw new Error(`There is no ${plantName} in the garden.`);
+    }
+
+    if (plantFound.ripe == true) {
+      throw new Error(`The ${plantName} is already ripe.`);
+    }
+
+    plantFound.ripe = true;
+    plantFound.quantity += quantity;
+
+    if (plantFound.quantity == 1) {
+      return `${quantity} ${plantName} has successfully ripened.`;
+    } else {
+      return `${quantity} ${plantName}s have successfully ripened.`;
+    }
+  }
+
+  harvestPlant(plantName) {
+    const plantIndex = this.plants.findIndex((p) => p.plantName == plantName);
+    // console.log(plantIndex);
+    if (plantIndex == -1) {
+      throw new Error(`There is no ${plantName} in the garden.`);
+    }
+    const plant = this.plants[plantIndex];
+    // console.log(plantName);
+    if (plant.ripe == false) {
+      throw new Error(
+        `The ${plantName} cannot be harvested before it is ripe.`
+      );
+    }
+
+    this.plants.splice(plantIndex, 1);
+    this.storage.push({
+      plantName,
+      quantity: plant.quantity,
+    });
+    // console.log(this.storage);
+    this.spaceAvailable += plant.spaceRequired;
+    return `The ${plantName} has been successfully harvested.`;
+  }
+
+  generateReport() {
+    // console.log(this.spaceAvailable);
+    
+    let storageRow = "Plants in storage: The storage is empty.";
+    if (this.storage.length > 0) {
+        const plantsInStorage = this.storage.map((p) => `${p.plantName} (${p.quantity})`);
+        storageRow = `Plants in storage: ${plantsInStorage.join(', ')}`;
+    }
+
+    const plantsAsString = this.plants.map((p) => p.plantName).sort((a, b) => a.localeCompare(b));
+    const plantsRow = `Plants in the garden: ${plantsAsString.join(", ")}`;
+
+    return [
+      `The garden has ${this.spaceAvailable} free space left.`,
+      plantsRow,
+      storageRow,
+    ].join("\n");
+  }
+}
+
+
+// const myGarden = new Garden(250)
+// console.log(myGarden.addPlant('apple', 20));
+// console.log(myGarden.addPlant('orange', 200));
+// console.log(myGarden.addPlant('olive', 50));
+
+// const myGarden = new Garden(250)
+// console.log(myGarden.addPlant('apple', 20));
+// console.log(myGarden.addPlant('orange', 100));
+// console.log(myGarden.addPlant('cucumber', 30));
+// console.log(myGarden.ripenPlant('apple', 10));
+// console.log(myGarden.ripenPlant('orange', 1));
+// console.log(myGarden.ripenPlant('orange', 4));
+
+// const myGarden = new Garden(250)
+// console.log(myGarden.addPlant('apple', 20));
+// console.log(myGarden.addPlant('orange', 200));
+// console.log(myGarden.addPlant('raspberry', 10));
+// console.log(myGarden.ripenPlant('apple', 10));
+// console.log(myGarden.ripenPlant('orange', 1));
+// console.log(myGarden.harvestPlant('apple'));
+// console.log(myGarden.harvestPlant('olive'));
+
+// const myGarden = new Garden(250)
+// console.log(myGarden.addPlant('apple', 20));
+// console.log(myGarden.addPlant('orange', 200));
+// console.log(myGarden.addPlant('raspberry', 10));
+// console.log(myGarden.ripenPlant('apple', 10));
+// console.log(myGarden.ripenPlant('orange', 1));
+// console.log(myGarden.harvestPlant('orange'));
+// // console.log(myGarden.harvestPlant('apple'));
+// // myGarden.generateReport()
+// console.log(myGarden.generateReport());
+
+/*
+report: 
+The garden has 220 free space left.
+Plants in the garden: apple, raspberry
+Plants in storage: orange (1) */
+
+const myGarden = new Garden(250);
+console.log(myGarden.addPlant("apple", 20));
+console.log(myGarden.addPlant("orange", 200));
+console.log(myGarden.addPlant("raspberry", 10));
+console.log(myGarden.ripenPlant("apple", 10));
+console.log(myGarden.ripenPlant("orange", 1));
+console.log(myGarden.harvestPlant("orange"));
+// console.log(myGarden.harvestPlant("raspberry"));
+console.log(myGarden.generateReport());
